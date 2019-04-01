52920b8fa77eef073d5a80d68eea9712d1199903
Amir Edin <amiredin@yahoo.com>
2019-04-01 14:54:41 +0300

    remove repository component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import { FormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubsearchComponent } from './githubsearch/githubsearch.component';
+import { RepositoryComponent } from './repository/repository.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubsearchComponent,
+    RepositoryComponent,
     
   ],
   imports: [
